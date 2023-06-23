4bffdc386b797134cd050908aa503c39f89c0157
batkhishig09 <batkhishig0009@yahoo.com>
2023-06-23 11:01:24 -0500

    Edit

--- app.js
+++ app.js
@@ -1,9 +1,95 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
+const postBank = require('./postBank'
+)
+app.use(morgan('dev'));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => res.send("Hello World!"));
 
-const PORT = 1337;
+
+app.get('/posts/:id', (req, res, next) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (!post.id) {
+    res.status(404)
+    const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class="not-found">
+        <h1>404: Page Not Found</h1>
+      </div>
+    </body>
+    </html>`
+    res.send(html)
+  } else {
+    const html = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+          <div class='news-item'>
+            <p>
+              <span class="news-position">${post.id}. ▲</span>
+              ${post.title}
+              <small>(by ${post.name})</small>
+            </p>
+            <br>
+            <span>
+            ${post.content} </span>
+            <br>
+  
+          </div>
+      </div>
+    </body>
+  </html>`
+    res.send(html);
+  }
+});
+  
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
+</html>`
+
+  res.send(html);
+});
+
+
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
---
+++ views/postList.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express();
+const postBank = require('../postBank')
+const posts = postBank.list();
+
+app.get('/', (req, res) => {
+    const html = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+        ${posts.map(post => `
+          <div class='news-item'>
+            <p>
+              <span class="news-position">${post.id}. ▲</span>
+              <a href="/posts/${post.id}">${post.title}</a>
+              <small>(by ${post.name})</small>
+            </p>
+            <small class="news-info">
+              ${post.upvotes} upvotes | ${post.date}
+            </small>
+          </div>`
+        ).join('')}
+      </div>
+    </body>
+  </html>`
+  res.send(html)
+})
+
+
